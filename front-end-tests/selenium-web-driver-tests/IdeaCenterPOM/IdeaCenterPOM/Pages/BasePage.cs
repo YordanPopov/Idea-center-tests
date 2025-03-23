28c5a6aef119c065cccf1b61866bd4399cefa775
YordanPopov <jordanpopov190@gmail.com>
2025-03-23 21:45:18 +0200

    initial commit

---
+++ front-end-tests/selenium-web-driver-tests/IdeaCenterPOM/IdeaCenterPOM/Pages/BasePage.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace IdeaCenterPOM.Pages
+{
+	public class BasePage
+	{
+		protected IWebDriver _driver;
+		protected WebDriverWait _wait;
+
+		public BasePage(IWebDriver driver)
+		{
+			this._driver = driver;
+			_wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+		}
+
+		public virtual string PageUrl { get; }
+
+		public IWebElement LinkLoginPage => _driver.FindElement(By.XPath("//a[contains(text(), 'Login')]"));
+
+		public IWebElement LinkRegisterPage => _driver.FindElement(By.XPath("//a[contains(text(), 'Sign up')]"));
+
+		public IWebElement LinkHomePage => _driver.FindElement(By.XPath("//a[@class='nav-link' and contains(text(), 'Idea Center')]"));
+
+		public void Open()
+		{
+			_driver.Navigate().GoToUrl(PageUrl);
+		}
+	}
+}
+
+// username: testUser_123
+// email: testUser_123@email.com
+// password: test1234
