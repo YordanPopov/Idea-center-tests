d029a5698511a432b268cad6a5988fe7096db8b0
YordanPopov <jordanpopov190@gmail.com>
2025-05-19 22:12:37 +0300

    Add goto method

--- front-end-tests/playwright-tests/pages/SignInPage.js
+++ front-end-tests/playwright-tests/pages/SignInPage.js
@@ -1,8 +1,13 @@
 export class SignInPage {
     constructor(page) {
+        this.page = page;
         this.email = page.locator('//input[@name="Email"]');
         this.password = page.locator('//input[@name="Password"]');
         this.signInBtn = page.locator('//button[@type="submit" and text()="Sign in"]');
+    }
+
+    async goto() {
+        await this.page.goto('http://softuni-qa-loadbalancer-2137572849.eu-north-1.elb.amazonaws.com:83/Users/Login');
     }
 
     async login({ email, password }) {
