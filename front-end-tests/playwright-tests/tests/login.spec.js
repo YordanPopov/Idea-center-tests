cee088d1e238a2ce3160851df9fd767f67f3124d
YordanPopov <jordanpopov190@gmail.com>
2025-05-21 22:06:53 +0300

    make some changes

--- front-end-tests/playwright-tests/tests/login.spec.js
+++ front-end-tests/playwright-tests/tests/login.spec.js
@@ -1,24 +1,17 @@
 import { test, expect } from '@playwright/test';
-import { SignInPage } from '../pages/SignInPage';
+import { SignInPage } from '../pages/SignInPage.js';
+import { existingUser, incorrectPassUser, unregisteredUser } from '../utils/testData.js';
 
 test.describe('Login Page tests', () => {
-    const existingUser = {
-        email: 'testUser_123@email.com',
-        password: 'test1234'
-    }
 
-    const incorrectPassUser = {
-        email: 'testUser_123@email.com',
-        password: 'invalidPassword'
-    }
-
-    test('Verify that user can successfully login with correct emai and password.', async ({ page }) => {
+    test('Verify that user can successfully login with correct email and password.', async ({ page }) => {
         const signInPage = new SignInPage(page);
 
         await signInPage.goto();
         await signInPage.login(existingUser);
 
-        await expect(page.locator('//a[@type="button"]')).toBeVisible();
+        const logoutBtn = await page.locator('//a[@type="button" and @href="/Users/Logout"]');
+        await expect(logoutBtn).toBeVisible();
     });
 
     test('Verify that user cannot login with empty fields.', async ({ page }) => {
@@ -28,9 +21,13 @@ test.describe('Login Page tests', () => {
 
         await signInPage.signInBtn.click();
 
-        await expect(page.locator('//div[@class="text-danger validation-summary-errors"]//li')).toContainText('Unable to sign in!');
-        await expect(page.locator('//span[@data-valmsg-for="Email"]')).toContainText('The e-mail is required!');
-        await expect(page.locator('//span[@data-valmsg-for="Password"]')).toContainText('The password is required!');
+        const mainErrorMsg = await page.locator('//div[@class="text-danger validation-summary-errors"]//li').textContent();
+        const emailErrorMsg = await page.locator('//span[@data-valmsg-for="Email"]').textContent();
+        const passErrorMsg = await page.locator('//span[@data-valmsg-for="Password"]').textContent();
+
+        expect(mainErrorMsg).toBe('Unable to sign in!');
+        expect(emailErrorMsg).toBe('The e-mail is required!');
+        expect(passErrorMsg).toBe('The password is required!');
     });
 
     test('Verify than user cannot login with incorrect password.', async ({ page }) => {
@@ -39,6 +36,18 @@ test.describe('Login Page tests', () => {
         await signInPage.goto();
         await signInPage.login(incorrectPassUser);
 
-        await expect(page.locator('//div[@class="text-danger validation-summary-errors"]//li')).toContainText('Unable to sign in!');
+        const errorMsg = await page.locator('//div[@class="text-danger validation-summary-errors"]/ul/li').textContent();
+        expect(errorMsg).toBe('Unable to sign in!');
+    });
+
+    test('Unregistered user cannot log in.', async ({ page }) => {
+        const signInPage = new SignInPage(page);
+
+        await signInPage.goto();
+        await signInPage.login(unregisteredUser);
+
+        const errorMsg = await page.locator('//div[@class="text-danger validation-summary-errors"]/ul/li').textContent();
+
+        expect(errorMsg).toBe('Unable to sign in!');
     });
 });
